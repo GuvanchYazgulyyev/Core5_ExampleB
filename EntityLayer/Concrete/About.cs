2be1e51f076a13477be93659ab855924fe006f3a
Guvanch <mr.yazgulyyew@outlook.com>
2022-04-11 00:14:40 +0300

    Proje dosyası ekle.

---
+++ EntityLayer/Concrete/About.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class About
+    {
+        [Key]
+        public int AboutID { get; set; }
+        public string AboutDetails { get; set; }
+        public string AboutImage { get; set; }
+        public string AboutMapLocation { get; set; }
+        public bool AboutStatus { get; set; }
+    }
+}
